e89e9b6f5e419eb7286e8ef0641f49443d30582d
Zinovii Dmytriv <zdmytriv@gmail.com>
2021-07-31 18:22:02 +0300

    update

--- src/metrics/metrics.go
+++ src/metrics/metrics.go
@@ -3,6 +3,7 @@ package metrics
 import (
 	"context"
 	stats "github.com/lyft/gostats"
+	logger "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 	"time"
 )
@@ -38,7 +39,9 @@ func (r *ServerReporter) UnaryServerInterceptor() func(ctx context.Context, req 
 		start := time.Now()
 		s := newServerMetrics(r.scope, info.FullMethod)
 		s.totalRequests.Inc()
+		start2 := time.Now()
 		resp, err := handler(ctx, req)
+		logger.Infof("%s execution time: %v milliseconds", info.FullMethod, float64(time.Since(start2).Milliseconds()))
 		s.responseTime.AddValue(float64(time.Since(start).Milliseconds()))
 		return resp, err
 	}
